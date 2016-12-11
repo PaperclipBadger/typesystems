bbd0e265da2967298e90ae3c0ccc995d6ac9c185
Blaine W Rogers <blaine.w.rogers@gmail.com>
2016-12-11 21:02:37 +0000

    Updated repl to reset free variables on each loop

--- typesystems/lambdacalculus/__init__.py
+++ typesystems/lambdacalculus/__init__.py
@@ -5,7 +5,10 @@ from . import *
 from .lexparse import parse
 
 def repl():
+    import string
     while True:
+        lambdacalculus.Variable.freshcounter = -1
+        lambdacalculus.Variable.freshletters = set(string.ascii_lowercase)
         term = parse(input('>>> '))
         try:
             while term.is_redex:
