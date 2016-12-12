fd59e179e078987c4689c76eb0c46fa60e489ad9
Blaine W Rogers <blaine.w.rogers@gmail.com>
2016-12-12 11:46:25 +0000

    repl -- now with unicode subscripts!

--- typesystems/__init__.py
+++ typesystems/__init__.py
@@ -4,6 +4,15 @@ from .currytypes import UnificationError, TypeVariable
 from .currytypeassignment import principal_pair
 
 def repl():
+    import re
+    def subscriptify(matchobj):
+        s = matchobj.group()
+        r = []
+        digits = "₀₁₂₃₄₅₆₇₈₉"
+        for c in s[1:]:
+            r.append(digits[int(c)])
+        return ''.join(r)
+
     while True:
         TypeVariable.freshcounter = -1
         term = parse(input('>>> '))
@@ -12,12 +21,18 @@ def repl():
         except UnificationError as e:
             print(e)
         else:
+            type_ = str(type_).replace('->', '⟶')
+            type_ = re.sub('_\d+', subscriptify, type_)
             print('Type: {}'.format(type_))
-            print('Context:')
-            for k, v in context.items():
-                print('  {} ↦ {}'.format(k, v))
-            print('Reduction:')
-            while term.is_redex:
-                print('  {}'.format(term))
-                term = term.reduce()
-            print('  {}'.format(term))
+            if context:
+                print('Context:')
+                for k, v in context.items():
+                    v_str = re.sub('_\d+', subscriptify, str(v))
+                    print('  {} ↦ {}'.format(k, v_str))
+            if term.is_redex:
+                print('Reduction:')
+                print('  {}'.format(re.sub('_\d+', subscriptify, str(term))))
+                while term.is_redex:
+                    term = term.reduce()
+                    term_str = re.sub('_\d+', subscriptify, str(term))
+                    print('  ⟶ᵦ {}'.format(term_str))
